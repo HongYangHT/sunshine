7554249b62bedab7ee528e7dcae7e023332bae58
superuser <superuser@xinyunlian06>
2015-01-12 14:59:45 +0800

    test index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { user:'' });
+  res.render('index', { user:{
+	username:'123',
+	messages:['a'],
+	imageUrl:'../images/head-m.gif'
+  }});
 });
 
 module.exports = router;
